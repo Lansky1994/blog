6af2e594ca1e367293e823df5a3606edc74a7a9d
Artsiom Lanksy <lansky1994@gmail.com>
2021-08-11 16:24:04 +0300

    add logout.php

--- profile.php
+++ profile.php
@@ -1,5 +1,8 @@
 <?php
 session_start();
+if(!$_SESSION['user']) {
+    header('Location: login.php');
+}
 ?>
 
 <!doctype html>
@@ -11,14 +14,13 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Профиль</title>
     <link rel="stylesheet" href="css/blog_login.css">
-
 </head>
 <body>
-
     <form>
         <img src="<?= $_SESSION['user']['avatar'] ?>" width="100" alt="">
         <h2><?= $_SESSION['user']['name']?></h2>
         <a href="#"><?= $_SESSION['user']['email']?></a>
+        <a href="vendor/logout.php" class="logout">Выход</a>
     </form>
 </body>
 </html>
